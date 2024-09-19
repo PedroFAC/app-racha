a09b9cadccd3f8d78ffacbc18cfe26d4e3cd76ba
Pedro Façanha <pedrofacpeix@gmail.com>
2024-09-19 20:23:52 -0300

    feat: finish timer visuals (mvp)

---
+++ constants/CommonStyles.ts
@@ -0,0 +1,17 @@
+import { TextStyle, ViewStyle } from "react-native";
+
+export const CommonStyles = {
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "gray",
+  } as ViewStyle,
+  buttonText: {
+    fontSize: 24,
+    color: "white",
+  } as TextStyle,
+};
